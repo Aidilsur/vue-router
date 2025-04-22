0b38b5182cbd704597f350fad5ab17ca5a9cc600
saidil <saidil@jakartamrt.co.id>
2025-04-22 12:00:24 +0700

    Programmatic Navigation

--- src/components/ProductSearch.vue
+++ src/components/ProductSearch.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { ref, watchEffect } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const products = ref([]);
 const route = useRoute();
-const search = route.query.product || "";
+const search = ref(route.query.product || "");
+const router = useRouter();
 
 watchEffect(() => {
+  router.replace({ query: { product: search.value } }); // replace or push
   fetch("/api/products.json")
     .then((response) => response.json())
     .then((products) =>
-      products.filter((product) => product.name.includes(search))
+      products.filter((product) => product.name.includes(search.value))
     )
     .then((data) => {
       products.value = data;
@@ -19,6 +21,16 @@ watchEffect(() => {
 </script>
 <template>
   <h1>Products</h1>
+  <form>
+    <input
+      type="text"
+      name="product"
+      v-model="search"
+      placeholder="Search products"
+    />
+  </form>
+  <h2>Search Results</h2>
+  <p v-if="products.length === 0">No products found</p>
   <table>
     <thead>
       <tr>
